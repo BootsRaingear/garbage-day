3447779014683ccf29a0667769d8d00e8eb5a014
Boots Raingear <bootsraingear@gmail.com>
2018-03-13 17:33:41 -0400

    obs controls add

---
+++ dashboard/elements/gd-obs-control/gd-obs-control.js
@@ -0,0 +1,40 @@
+(function () {
+	const onBreak = nodecg.Replicant('onBreak');
+	const currentLayout = nodecg.Replicant('currentLayout');
+	
+	class GdObsControl extends Polymer.Element {
+		static get is() {
+			return 'gd-obs-control';
+		}
+		
+		static get properties() {
+			return {
+				onBreak: {
+					type: Boolean,
+					value: false,					
+				},
+				disabled: {
+					type: Boolean,
+					value false,
+					reflectToAttribute: true
+				}
+			};
+		}
+		
+		ready() {
+			super.ready();
+			onBreak.on('change', newVal => {
+				this.onBreak = newVal;
+			});
+		}
+		
+		startBreak() {
+			onBreak.value = true;
+		}
+		
+		endBreak() {
+			onBreak.value = false;
+		}
+		
+		customElements.define(GdObsControl.is, GdObsControl);		
+	})();
