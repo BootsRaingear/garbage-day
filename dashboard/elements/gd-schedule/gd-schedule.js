fb1f1bd5d086336393b8a8259887601cc6bdef85
Boots Raingear <bootsraingear@gmail.com>
2018-03-15 23:42:11 -0400

    Segment array replicant

--- dashboard/elements/gd-schedule/gd-schedule.js
+++ dashboard/elements/gd-schedule/gd-schedule.js
@@ -1,6 +1,7 @@
 (function () { 
 	
 	const currentSegment = nodecg.Replicant('currentSegment');
+	const segments = nodecg.Replicant('segments');
 
 	class GdSchedule extends Polymer.MutableData(Polymer.Element) {
 		static get is() {
@@ -21,41 +22,73 @@
 				ridiculist6: String,
 				artistName: String,
 				artistURL: String,
-				streamTwoName: String,
-				streamTwoURL: String			
+				streamtwoName: String,
+				streamtwoURL: String			
 			};
 		}
 		
 		fetch() {
-			this.hour = currentSegment.value.hour;
-			this.title = currentSegment.value.title;
-			this.docProvider = currentSegment.value.docProvider;
-			this.docURL = currentSegment.value.docURL;
-			this.ridiculist1 = currentSegment.ridiculist1;
-			this.ridiculist2 = currentSegment.ridiculist2;
-			this.ridiculist3 = currentSegment.ridiculist3;
-			this.ridiculist4 = currentSegment.ridiculist4;
-			this.ridiculist5 = currentSegment.ridiculist5;
-			this.ridiculist6 = currentSegment.ridiculist6;
-			this.artistName = currentSegment.value.artistName;
-			this.artistURL = currentSegment.value.artistURL;
-
+			if (1 <= this.hour && this.hour <= 24) {
+				if (typeof segments.value[this.hour] !== 'undefined') {
+					console.log("found it!");
+					currentSegment.value = segments.value[this.hour];
+					this.title = currentSegment.value.title;
+					this.docProvider = currentSegment.value.docProvider;
+					this.docURL = currentSegment.value.docURL;
+					this.ridiculist1 = currentSegment.value.ridiculist1;
+					this.ridiculist2 = currentSegment.value.ridiculist2;
+					this.ridiculist3 = currentSegment.value.ridiculist3;
+					this.ridiculist4 = currentSegment.value.ridiculist4;
+					this.ridiculist5 = currentSegment.value.ridiculist5;
+					this.ridiculist6 = currentSegment.value.ridiculist6;
+					this.artistName = currentSegment.value.artistName;
+					this.artistURL = currentSegment.value.artistURL;
+					this.streamtwoName = currentSegment.value.streamtwoName;
+					this.streamtwoURL = currentSegment.value.streamtwoURL;
+				} else {
+					console.log("not found!");
+					this.title = "";
+					this.docProvider = "";
+					this.docURL = "";
+					this.ridiculist1 = "";
+					this.ridiculist2 = "";
+					this.ridiculist3 = "";
+					this.ridiculist4 = "";
+					this.ridiculist5 = "";
+					this.ridiculist6 = "";
+					this.artistName = "";
+					this.artistURL = "";
+					this.streamtwoName = "";
+					this.streamtwoURL = "";
+				}
+			} else {
+				console.log("[garbage-day] schedule.js, fetch() - hour out of range, idiot");
+			}
+			
 		}
 		
 		update() {
-			currentSegment.value.hour = this.hour;
-			currentSegment.value.title = this.title;
-			currentSegment.value.docProvider = this.docProvider;
-			currentSegment.value.docURL = this.docURL;
-			currentSegment.value.ridiculist1 = this.ridiculist1;
-			currentSegment.value.ridiculist2 = this.ridiculist2;
-			currentSegment.value.ridiculist3 = this.ridiculist3;
-			currentSegment.value.ridiculist4 = this.ridiculist4;
-			currentSegment.value.ridiculist5 = this.ridiculist5;
-			currentSegment.value.ridiculist6 = this.ridiculist6;
-			currentSegment.value.artistName = this.artistName;
-			currentSegment.value.artistURL = this.artistURL;			
-			
+			if (1 <= this.hour && this.hour <= 24)
+			{
+				currentSegment.value.hour = this.hour;
+				currentSegment.value.title = this.title;
+				currentSegment.value.docProvider = this.docProvider;
+				currentSegment.value.docURL = this.docURL;
+				currentSegment.value.ridiculist1 = this.ridiculist1;
+				currentSegment.value.ridiculist2 = this.ridiculist2;
+				currentSegment.value.ridiculist3 = this.ridiculist3;
+				currentSegment.value.ridiculist4 = this.ridiculist4;
+				currentSegment.value.ridiculist5 = this.ridiculist5;
+				currentSegment.value.ridiculist6 = this.ridiculist6;
+				currentSegment.value.artistName = this.artistName;
+				currentSegment.value.artistURL = this.artistURL;
+				currentSegment.value.streamtwoName = this.streamtwoName;
+				currentSegment.value.streamtwoURL = this.streamtwoURL;
+				segments.value[this.hour]  = currentSegment.value;
+			} else {
+				console.log("[garbage-day] schedule.js, update() - hour out of range, idiot");
+			}
+				
 		}
 		
 	}
