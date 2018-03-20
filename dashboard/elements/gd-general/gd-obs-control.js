c8fa3975b0b2456e32671e0d707ea26e7ea36032
PSY\pneill <boots@thefpl.us>
2018-03-20 13:38:54 -0400

    Hour checker added

---
+++ dashboard/elements/gd-general/gd-obs-control.js
@@ -0,0 +1,45 @@
+(function () {
+	const onBreak = nodecg.Replicant('onBreak');
+	const sceneList = nodecg.Replicant('obs:sceneList');
+	
+	class GdObsControl extends Polymer.Element {
+		static get is() {
+			return 'gd-obs-control';
+		}
+		
+		static get properties() {
+			return {
+				onBreak: {
+					type: Boolean,
+					value: false,					
+				},
+				obsScenes: Array,
+			}
+		};				
+		
+		ready() {
+			super.ready();
+			onBreak.on('change', newVal => {
+				this.onBreak = newVal;
+			});
+			sceneList.on('change', newVal => {
+				this.scenes = newVal;
+			});
+		}
+		
+		startBreak() {
+			onBreak.value = true;
+		}
+		
+		endBreak() {
+			onBreak.value = false;
+		}
+		
+		/*
+		_handleSelectScene(e) {
+			//nodecg.sendMessage('obsChangeScene', e.model.obsScene.base);
+		}
+		*/	
+	}
+	customElements.define(GdObsControl.is, GdObsControl);			
+})();
---
+++ extension/general.js
@@ -0,0 +1,15 @@
+'use strict';
+const nodecg = require('./util/nodecg-api-context').get();
+const cron =  require('node-schedule');
+const currentHour = nodecg.Replicant('currentHour');
+
+var changeHour = cron.scheduleJob('0 * * * *', function() {	
+	if (Date.now() > nodecg.config.marathonStart) {
+		var newHour = round((Date.now() - nodecg.config.marathonStart) / (1000 * 60 * 60));
+		currentHour.value = newHour;
+		nodecg.log.info("It is now hour: " + newHour);
+	} else 	{
+		currentHour.value = 0;
+		nodecg.log.info("Hour check: marathon has not yet started");
+	}
+}
