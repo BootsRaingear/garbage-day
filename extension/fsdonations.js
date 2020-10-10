3d52fc0c408449376063f2d67b3b85f48efa0ca5
Boots Raingear <bootsraingear@gmail.com>
2020-10-10 11:51:23 -0400

    formatted time in donation grab

--- extension/fsdonations.js
+++ extension/fsdonations.js
@@ -14,14 +14,15 @@ let db = new sqlite3.Database('/srv/gd2020nnaf/gd_nnaf.db', (err) => {
 	nodecg.log.info('Connected to the database.');
   });
 
-if (lastDonationTime.value < nodecg.bundleConfig.marathonStart) {
+//if (lastDonationTime.value < nodecg.bundleConfig.marathonStart) {
   lastDonationTime.value = nodecg.bundleConfig.marathonStart;
-}
+//}
 
 GrabRecentDonations();
+GetDonationTotal();
 
 function GrabRecentDonations() {
-	let query = "SELECT id, displayname, message, amount, STRFTIME('%s', timestamp) AS dtime FROM Donations WHERE dtime > " + lastDonationTime.value + " ORDER BY id DESC LIMIT 25";
+	let query = "SELECT id, displayname, message, amount, STRFTIME('%s', timestamp) AS dtime, STRFTIME('%H:%M:%S') AS ftime FROM Donations WHERE dtime > " + lastDonationTime.value + " ORDER BY id DESC LIMIT 25";
 	db.all(query, [], (err, rows) => {
 		if (err) {
 			throw err;
@@ -33,14 +34,22 @@ function GrabRecentDonations() {
 }
 
 function GetDonationTotal() {
-	let query = "SELECT SUM(amount) as total FROM Donations WHERE dtime > " + nodecg.bundleConfig.marathonStart;
+	let query = "SELECT SUM(amount) AS total FROM Donations WHERE STRFTIME('%s', timestamp) > " + nodecg.bundleConfig.marathonStart;
 	var total = -1;
 	db.all(query, [], (err, rows) => {
 		if(err) {
-			console.log(err);
+			nodecg.log.info(err);
 		}else{
 			total = rows[0].total;
+			nodecg.log.info(rows[0].total);
+			donationTotal.value = total;
 		}
 	});
-	donationTotal.value = total;
 }
+
+// grab recent donations from streamlabs api every minute
+setInterval(() => {
+	nodecg.log.info('[donations] Grabbings recent donations from db');
+	GrabRecentDonations();
+	GetDonationTotal();
+}, 5 * 1000);
