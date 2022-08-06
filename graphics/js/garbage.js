9941e6d455b091c029a82c5c0f2d6b9a4432eb9e
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2022-08-06 16:54:40 -0500

    Feature complete, needs better animation.

--- graphics/js/garbage.js
+++ graphics/js/garbage.js
@@ -1,2 +1,3 @@
 //@prepros-append partials/_functions.js
+//@prepros-append partials/_variables.js
 //@prepros-append partials/_vue.js
---
+++ graphics/js/partials/_variables.js
@@ -0,0 +1,28 @@
+const relativeValues = [
+  {
+    name: "Bulletproof Vibe Whole-Body Vibration Plate",
+    plural: "Whole-Body Vibration Plates",
+    cost: 1495,
+    pic: "vibration-plate.jpg"
+  },
+  {
+    name: "Goop Brand Rose Quartz Egg",
+    plural: "Rose Quartz Eggs from Goop",
+    cost: 55,
+    notes: "(item is non-refundable)",
+    pic: "rose-quartz-egg.jpg"
+  },
+  {
+    name: "Night Watchman Blackthorn Shillelagh Walking Stick",
+    plural: "Personal Defense Shillelaghs from BudK.com",
+    cost: 47,
+    pic: "shelaylee.jpg"
+  },
+  {
+    name: "Orgonite Reiki Healing Pyramid",
+    plural: "Orgonite Reiki Healing Pyramids",
+    cost: 34,
+    pic: "healing-pyramid.jpg",
+    notes: "(cleanses your chakra)"
+  }
+]
